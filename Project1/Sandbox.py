a1991fb126e4d26b7de8cc4621fd6245fe8f9a94
Brian J. Poole <bpoole32@gatech.edu>
2022-01-26 17:46:48 -0500

    Create Sandbox.py
    Sandbox to try various ideas

---
+++ Project1/Sandbox.py
@@ -0,0 +1,19 @@
+
+
+# tmpTest = [[0] * 10] * 5
+#
+# print(tmpTest)
+# print(type(tmpTest))
+#
+# for i in range(0, 5):
+#     print(tmpTest[i][0])
+
+
+# print([item for item in range(0, 11)])
+#
+# a = 2
+# b = 4
+#
+# print(max(a, b))
+#
+# print(range(10))
