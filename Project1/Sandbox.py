85c428f08fa49880aadc93399824ef144556b4a5
Brian J. Poole <bpoole32@gatech.edu>
2022-01-26 21:00:27 -0500

    Update Sandbox.py
    More sample code

--- Project1/Sandbox.py
+++ Project1/Sandbox.py
@@ -1,8 +1,16 @@
 
 
 # tmpTest = [[0] * 10] * 5
+
+# tmpTest = [[0] * 10 for i in range(5)]
+#
+# tmpTest[0][1] = 1
+# tmpTest[4][3] = 6
+# tmpTest[2][1] = 3
+# tmpTest[4][9] = 11
 #
 # print(tmpTest)
+
 # print(type(tmpTest))
 #
 # for i in range(0, 5):
@@ -17,3 +25,14 @@
 # print(max(a, b))
 #
 # print(range(10))
+
+
+tmpList = [1,2,3,4,5,6]
+
+print(tmpList)
+
+tmpList.insert(0, 1337)
+
+print(tmpList)
+
+print(1 !=14)
